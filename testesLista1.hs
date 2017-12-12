fe5df1e88c9851f93d6490bba6d5d1d82eb5d6b4
jaaziellevi <jaaziel.moura@ccc.ufcg.edu.br>
2017-12-12 20:40:51 -0300

    Alguns testes para square, pow e fatorial

--- testesLista1.hs
+++ testesLista1.hs
@@ -55,3 +55,40 @@ t2Mmc = TestCase (assertEqual "t2" 0 (mmc 0 20))
 t3Mmc = TestCase (assertEqual "t3" 12 (mmc 4 6))
 
 testesMmc = TestList [t1Mmc, t2Mmc, t3Mmc]
+
+---------------------------------------------------------------------
+
+t1Square = TestCase (assertEqual "t" 100 (square (-10)))
+t2Square = TestCase (assertEqual "t" 25 (square (-5)))
+t3Square = TestCase (assertEqual "t" 1 (square (-1)))
+t4Square = TestCase (assertEqual "t" 0 (square (0)))
+t5Square = TestCase (assertEqual "t" 1 (square (1)))
+t6Square = TestCase (assertEqual "t" 25 (square (5)))
+t7Square = TestCase (assertEqual "t" 100 (square (10)))
+
+testesSquare = TestList [t1Square, t2Square, t3Square, t4Square, t5Square, t6Square, t7Square]
+
+---------------------------------------------------------------------
+
+t1Pow = TestCase (assertEqual "t" 1 (pow 2 0))
+t2Pow = TestCase (assertEqual "t" 2 (pow 2 1))
+t3Pow = TestCase (assertEqual "t" (-8) (pow (-2) 3))
+t4Pow = TestCase (assertEqual "t" 0.2 (pow 5 (-1)))
+t5Pow = TestCase (assertEqual "t" 1 (pow 1 (-1)))
+t6Pow = TestCase (assertEqual "t" (-1) (pow (-1) (-1)))
+t7Pow = TestCase (assertEqual "t" 1e-10 (pow (-10) (-10)))
+
+testesPow = TestList [t1Pow, t2Pow, t3Pow, t4Pow, t5Pow, t6Pow, t7Pow]
+
+---------------------------------------------------------------------
+
+t1Fatorial = TestCase (assertEqual "t" 1 (fatorial 0))
+t2Fatorial = TestCase (assertEqual "t" 120 (fatorial 5))
+t3Fatorial = TestCase (assertEqual "t" 5040 (fatorial 7))
+t4Fatorial = TestCase (assertEqual "t" 362880 (fatorial 9))
+t5Fatorial = TestCase (assertEqual "t" 3628800 (fatorial 10))
+t6Fatorial = TestCase (assertEqual "t" 2432902008176640000 (fatorial 20))
+
+testesFatorial = TestList [t1Fatorial, t2Fatorial, t3Fatorial, t4Fatorial, t5Fatorial, t6Fatorial]
+
+---------------------------------------------------------------------
